c0943ff44a505455a0807a66539d6a73a9585843
AEDD1970 <ing.alexis.duque@gmail.com>
2020-04-01 19:56:01 -0500

    ajust in route for db in database.js

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
 import mongose from 'mongoose';
 
  export async function startConection(){
- await  mongose.connect('mongodb://localhost/photo-gallery-db', {
+ await  mongose.connect('mongodb+srv://photo:22985032@photo-database-f81kt.mongodb.net/test?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useFindAndModify: false
     });
